605de28c832f948115cc6137be3b6d8ed95d0981
Meysam Mahmoudi <meisamdev@gmail.com>
2023-05-05 21:25:50 +0330

    Add Projects

---
+++ Meisam.DatatablesNet.BlazorHybrid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Meisam.DatatablesNet.BlazorHybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Meisam.DatatablesNet.BlazorHybrid/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Meisam.DatatablesNet.BlazorHybrid
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
